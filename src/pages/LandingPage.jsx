b6635cfafe14eec4c9195e14fe59328784f1f60c
Jagroshan <jagroshansingh615@gmail.com>
2023-09-24 07:52:40 +0530

    initialised a vite react app

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/LandingPage.css'
+
+export const LandingPage = () => {
+  return (
+    <div>
+        <div id='BackgroundVideo'>
+        </div>
+    </div>
+  )
+}
