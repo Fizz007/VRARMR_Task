1593447f84f16b159931ae45b3a489e745e31c5f
Jagroshan <jagroshansingh615@gmail.com>
2023-09-24 18:18:15 +0530

    lowerHalf part of NextPage is left with endNavigation

--- src/pages/NextPage.jsx
+++ src/pages/NextPage.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import './css/NextPage.css'
+import React from "react";
+import "./css/NextPage.css";
 
 export const NextPage = () => {
   return (
     <div>
-      <div id='backgroundImage'>
+      <div id="backgroundImage">
         <img src="./src/uiAssets/background.png" alt="" />
       </div>
 
-      <div id='absolutePosition'>
-        <div id='lowerHalf'>
-          
+      <div id="absolutePosition">
+        <div id="lowerHalf">
+          <div>
+            <img src="./src/uiAssets/rightarrow.png" alt="" />
+            <img src="./src/uiAssets/leftarrow.png" alt="" />
+          </div>
+          <div>
+            <h2>Digital Interface</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+              voluptate quidem aut similique quam enim harum, nostrum vitae
+              veritatis voluptas delectus possimus earum, voluptatum, sapiente
+              sunt. A nihil enim necessitatibus.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
